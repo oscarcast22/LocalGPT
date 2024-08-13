bc75c8be5e9dcc8ce602325ca26092e5c637ddb1
oscarcast22 <oscar_cm96@outlook.com>
2024-08-13 11:13:06 -0600

    .

--- public/workers/worker.js
+++ public/workers/worker.js
@@ -1,4 +1,4 @@
-import { WebWorkerMLCEngineHandler } from "https://esm.run/@mlc-ai/web-llm"
+import { WebWorkerMLCEngineHandler } from "https://cdn.jsdelivr.net/npm/@mlc-ai/web-llm@0.2.59/lib/index.min.js"
 
 const handler = new WebWorkerMLCEngineHandler()
 
